1d26f5d1d9cc0a391a2d246a782457999d5b9cda
suzuuuuu09 <bsamu5788@gmail.com>
2024-07-28 19:03:03 +0900

    feat: ScrambleModeの追加

--- text_displayer.py
+++ text_displayer.py
@@ -34,7 +34,7 @@ class TextDisplayer:
             print(line)
             sleep(interval)
 
-    # n秒おきにテキストを表示後、入力させる
+    # n秒おきにテキストを表示後,入力させる
     def inputI(self, text: str, interval: float):
         texts = list(text)
         for text in texts:
@@ -43,7 +43,7 @@ class TextDisplayer:
         input_content = input("")
         return input_content
 
-    # n秒間かけてテキストを表示後、入力させる
+    # n秒間かけてテキストを表示後,入力させる
     def inputD(self, text: str, duration: float):
         texts = list(text)
         interval = duration / len(texts)
@@ -52,3 +52,32 @@ class TextDisplayer:
             sleep(interval)
         input_content = input("")
         return input_content
+
+    # n秒おきにテキストを表示する.表示前にランダムな文字を出す.(日本語は使えない)
+    def printScrambleI(self, text: str, interval: float):
+        texts = list(text)
+        for i in range(len(texts)):
+            parameter_text = " "
+            try: 
+                real_interval = interval / (ord(texts[i]) - ord(parameter_text))
+            except ZeroDivisionError:
+                real_interval = interval
+            while parameter_text <= texts[i]:
+                print(f"\033[0K\r{text[0:i]}{parameter_text}", flush=True, end="")
+                parameter_text = chr(ord(parameter_text) + 1)
+                sleep(real_interval)
+
+    # n秒かけてテキストを表示する.表示前にランダムな文字を出す.(日本語は使えない)
+    def printScrambleD(self, text: str, duration: float):
+        texts = list(text)
+        interval = duration / len(texts)
+        for i in range(len(texts)):
+            parameter_text = " "
+            try:
+                real_interval = interval / (ord(texts[i]) - ord(parameter_text))
+            except ZeroDivisionError:
+                real_interval = interval
+            while parameter_text <= texts[i]:
+                print(f"\033[0K\r{text[0:i]}{parameter_text}", flush=True, end="")
+                parameter_text = chr(ord(parameter_text) + 1)
+                sleep(real_interval)
