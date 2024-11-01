5f1ee90f40c7c56c811166da746f26ca4971750e
suzu <140299273+suzuuuuu09@users.noreply.github.com>
2024-11-01 18:53:08 +0900

    Update choice_selector.py

--- choice_selector.py
+++ choice_selector.py
@@ -1,31 +1,32 @@
 from msvcrt import getch
 
 class ChoiceSelector:
-    def __init__(self, options, start_row):
-        self.options = options
-        self.selected_index = 0
-        self.start_row = start_row
+    def __init__(self, menu_options, menu_start_row, menu_selected_index=0):
+        self.menu_options = menu_options
+        self.menu_selected_index = menu_selected_index
+        self.menu_start_row = menu_start_row
 
     def display_menu(self):
-        for i, option in enumerate(self.options):
-            if i == self.selected_index:
-                print(f"\033[{self.start_row};{i*10 + 5}H\033[32m> {option}\033[0m", end='', flush=True)
+        print("\033[2J")  # Clear the screen
+        for i, option in enumerate(self.menu_options):
+            if i == self.menu_selected_index:
+                print(f"\033[{self.menu_start_row};{i*10 + 5}H\033[32m> {option}\033[0m", end='', flush=True)
             else:
-                print(f"\033[{self.start_row};{i*10 + 5}H  {option}", end='', flush=True)
+                print(f"\033[{self.menu_start_row};{i*10 + 5}H  {option}", end='', flush=True)
 
     def run(self):
         self.display_menu()
         while True:
             key = ord(getch())
             if key == 75:  # Left arrow key
-                if self.selected_index > 0:
-                    self.selected_index -= 1
+                if self.menu_selected_index > 0:
+                    self.menu_selected_index -= 1
                     self.display_menu()
             elif key == 77:  # Right arrow key
-                if self.selected_index < len(self.options) - 1:
-                    self.selected_index += 1
+                if self.menu_selected_index < len(self.menu_options) - 1:
+                    self.menu_selected_index += 1
                     self.display_menu()
             elif key == 13:  # Enter key
                 break
         print("\n")
-        return self.options[self.selected_index]
+        return self.menu_options[self.menu_selected_index]
